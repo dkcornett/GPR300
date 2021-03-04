0429ad3b41dcde3f02787fffca8d0928dc1189f1
Dianna <dianna.cornett@mymail.champlain.edu>
2021-03-04 14:17:07 -0500

    follow along from 3/4

--- animal3D SDK/resource/glsl/4x/fs/02-pipeline-deferred/postDeferredShading_fs4x.glsl
+++ animal3D SDK/resource/glsl/4x/fs/02-pipeline-deferred/postDeferredShading_fs4x.glsl
@@ -43,10 +43,53 @@ in vec4 vTexcoord_atlas;
 
 uniform int uCount;
 
+uniform sampler2D uImage00;	//diffuse
+uniform sampler2D uImage01;	// specular atlas
+
+uniform sampler2D uImage04;	//texcoords
+uniform sampler2D uImage05;	//normals
+uniform sampler2D uImage06;	//"position"
+uniform sampler2D uImage07;	//depth
+
+
+
+
+
+/// testing
+//uniform sampler2D uImage02, uImage03;
+
 layout (location = 0) out vec4 rtFragColor;
 
 void main()
 {
 	// DUMMY OUTPUT: all fragments are OPAQUE ORANGE
-	rtFragColor = vec4(1.0, 0.5, 0.0, 1.0);
+	//rtFragColor = vec4(1.0, 0.5, 0.0, 1.0);
+
+	// Phong:
+	// ambient
+	// + diffuse color * diffuse light
+	// + specular color * specular light
+	
+	//have:
+	// -> diffus e& specular COLOR -> atkases
+	//have not: 
+	//	-> light data
+	//	-> model/scene data -> "attributes"
+	//		-> gbuffer
+
+	// draw "objects" with "diffuse texture" applied
+	//  -> screen space coordinate to spample
+	//	uImage04(scene texcoord)
+	// -> we also have the texture
+	// - > sample atlas using sceneTexoord
+	vec4 sceneTexcoord = texture(uImage04, vTexcoord_atlas.xy);
+	vec4 diffuseSample = texture(uImage00, sceneTexcoord.xy);
+	
+	// DEBUGGING
+	//rtFragColor = vTexCoord_atlas;
+	//rtFragColor = texture(uImage00, vTexcoord_atlas);
+//	rtFragColor = texture(uImage01, vTexcoord_atlas);
+	//rtFragColor = sceneTexcoord;
+	rtFragColor = diffuseSample;
+
 }
--- animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode2_SSFX/a3_DemoMode2_SSFX-idle-update.c
+++ animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode2_SSFX/a3_DemoMode2_SSFX-idle-update.c
@@ -45,10 +45,11 @@ void a3ssfx_update_graphics(a3_DemoState* demoState, a3_DemoMode2_SSFX* demoMode
 	//	-> uncomment transformation and light data uploads
 	//	-> add line to upload light transformations
 	//		(hint: just individual matrices, see scene update)
-/*	// upload
+	// upload
 	a3bufferRefillOffset(demoState->ubo_transform, 0, 0, sizeof(demoMode->modelMatrixStack), demoMode->modelMatrixStack);
 	a3bufferRefillOffset(demoState->ubo_light, 0, 0, sizeof(demoMode->pointLightData), demoMode->pointLightData);
-	//...*/
+//	a3bufferRefillOffset(demoState->ubo_mvp, 0,0, sizeof(demoMode->);
+	//...
 }
 
 void a3ssfx_update_scene(a3_DemoState* demoState, a3_DemoMode2_SSFX* demoMode, a3f64 const dt)
@@ -128,8 +129,9 @@ void a3ssfx_update_scene(a3_DemoState* demoState, a3_DemoMode2_SSFX* demoMode, a
 		//		(hint: in the previous line, we calculate the view-space position)
 		//		(hint: determine the scale part, append position and multiply by 
 		//			projection matrix to arrive at a proper MVP for each light)
-	/*	// update and transform light matrix
-		//...*/
+		// update and transform light matrix
+
+		//...
 	}
 }
 
